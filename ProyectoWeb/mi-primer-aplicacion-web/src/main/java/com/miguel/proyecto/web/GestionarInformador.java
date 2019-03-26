c28155d728f109881c1c0914e575e267755a17ff
Jaime García <jarturoga@hotmail.com>
2019-03-26 13:23:52 -0600

    Update GestionarInformador.java

--- ProyectoWeb/mi-primer-aplicacion-web/src/main/java/com/miguel/proyecto/web/GestionarInformador.java
+++ ProyectoWeb/mi-primer-aplicacion-web/src/main/java/com/miguel/proyecto/web/GestionarInformador.java
@@ -2,13 +2,18 @@ public class GestionarInformador {
        
     Informador informador;
   
-    public GestionadorInformador() {
-        informador = new Informador();
-        
+    public GestionadorInformador(Informador informador) {
+        this.informador = informador;
     }
   
+    public void eliminarInformador() {
+        this.informador = null;       
+    }
+    
+    public void Nombre(String nombre) }
+        this.informador.setNombre(nombre); 
+    }
   
-  
-  
+    public void 
   
 }
