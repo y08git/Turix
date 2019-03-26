ac2fe3226d9b5dc94dbe7159bc3ea50d076361bf
Jaime García <jarturoga@hotmail.com>
2019-03-26 13:35:34 -0600

    Create Marcador.java

---
+++ ProyectoWeb/mi-primer-aplicacion-web/src/main/java/com/miguel/proyecto/web/Marcador.java
@@ -0,0 +1,59 @@
+public class Marcador {
+    
+    private int id;
+    private int calificacion;
+    private String descripcion;
+    private LinkedList<Comentario>();
+    private String titulo;
+    private String fecha;
+
+    public Marcador(int id, int calificacion, String descripcion, String titulo, String fecha) {
+        this.id = id;
+        this.calificacion = calificacion;
+        this.descripcion = descripcion;
+        this.titulo = titulo;
+        this.fecha = fecha;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getCalificacion() {
+        return calificacion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCalificacion(int calificacion) {
+        this.calificacion = calificacion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+    
+    
+}
