347273d2071d0f8ff01f40ac60081ab3bb6370bc
Yair Chiu <y08world@gmail.com>
2019-04-04 14:10:00 -0600

    Cambios a la base de datos

--- Database/database.sql
+++ Database/database.sql
@@ -21,7 +21,7 @@ CREATE TABLE notitia.Usuario
 drop table if exists notitia.Temas;
 CREATE TABLE notitia.Temas
 (
-  nombre INT NOT NULL,
+  nombre text NOT NULL,
   descripcion text NOT NULL,
   PRIMARY KEY (nombre)
 );
@@ -33,7 +33,7 @@ CREATE TABLE notitia.Marcadores
   descripcion text NOT NULL,
   ubicacion text NOT NULL,
   nombre_usuario text NOT NULL,
-  nombre INT NOT NULL,
+  nombre text NOT NULL,
   PRIMARY KEY (ubicacion),
   FOREIGN KEY (nombre_usuario) REFERENCES notitia.Usuario(nombre_usuario),
   FOREIGN KEY (nombre) REFERENCES notitia.Temas(nombre)
@@ -53,7 +53,7 @@ CREATE TABLE notitia.Comentarios
   PRIMARY KEY (id_comentario, ubicacion),
   FOREIGN KEY (ubicacion) REFERENCES notitia.Marcadores(ubicacion),
   FOREIGN KEY (nombre_usuario) REFERENCES notitia.Usuario(nombre_usuario)
-); /*  INSERT INTO notitai.usuario (nombre_usuario, contraseña, correo, es_informador)
+); /*  INSERT INTO notitia.usuario (nombre_usuario, contraseña, correo, es_informador)
 		VALUES ('Yo','password','asdfasd@adds',false)	*/
 
 
