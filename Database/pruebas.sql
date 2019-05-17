278d073d1f6ca2db2c5cf3ee5caf1b11fe366007
Jaime García <jarturoga@hotmail.com>
2019-05-17 16:27:32 -0500

    oki

--- Database/pruebas.sql
+++ Database/pruebas.sql
@@ -1,6 +1,8 @@
 INSERT INTO notitia.usuario (nombre_usuario, en_espera, contraseña, correo, es_informador)
 VALUES ('Yo', false, 'password','asdfas',false);
 
+INSERT INTO notitia.usuario (nombre_usuario, en_espera, contraseña, correo, es_informador)
+VALUES ('Admin', false, 'contraseña','turixteam2019@gmail.com',false);
 
 INSERT INTO notitia.temas (nombre, descripcion)
 VALUES ('rigoberto','magic');
