ce0c9d9e83391c44f0f0937c1e049d2e01c323f7
dianis02 <dianis0297@gmail.com>
2019-05-31 22:02:38 -0500

    marcadores prueba base

--- Database/pruebas.sql
+++ Database/pruebas.sql
@@ -8,6 +8,10 @@ VALUES ('Admin', false, 'contraseña','turixteam2019@gmail.com',false);
 INSERT INTO notitia.usuario (nombre_usuario, en_espera, contraseña, correo, es_informador)
 VALUES ('wait', true, 'contraseña','wait@gmail.com',false);
 
+INSERT INTO notitia.usuario (nombre_usuario, en_espera, contraseña, correo, es_informador)
+VALUES ('info', true, 'contraseña','info@gmail.com',true);
+
+
 INSERT INTO notitia.temas (nombre, descripcion)
 VALUES ('rigoberto','magic');
 INSERT INTO notitia.temas (nombre, descripcion)
@@ -18,7 +22,8 @@ INSERT INTO notitia.temas (nombre, descripcion)
 VALUES ('canto','mahou');
 
 INSERT INTO notitia.marcadores (datos_utiles, descripcion, ubicacion, nombre_usuario, nombre)
-VALUES ('datos inutiles','much impressive WOW','2342.23423,23423.34242','Yo','rigoberto');
+VALUES ('marcador1','descripcion1','19.326480,-99.179080','Yo','roberto');
 
 INSERT INTO notitia.marcadores (datos_utiles, descripcion, ubicacion, nombre_usuario, nombre)
-VALUES ('rigoberto','magic','42','Yo','roberto');
+VALUES ('marcador2','descripcion2','19.327178,-99.182041','Yo','roberto');
+
